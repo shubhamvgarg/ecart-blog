bc2a0f062739dbb9ff5eec8df624d90aaf004be4
Shubham Garg <shubham07119garg@gmail.com>
2020-06-17 17:55:38 +0530

    Add some migrations and temp paths

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "ShopHome")
+    path("", views.index, name = "ShopHome"), 
+    path("about/", views.about, name = "AboutUs"),
+    path("contact/", views.contact, name = "ContactUs"),
+    path("tracker/", views.tracker, name = "TreakingStatus"),
+    path("search/", views.search, name = "Search"),
+    path("prductview/", views.productView, name = "ProductView"),
+    path("chechout/", views.checkout, name = "Checkout"),
+
+
+
+    
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
-    
+
+def about(request):
+    return HttpResponse("We are at about")
+
+def contact(request):
+    return HttpResponse("We are at contact")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def productView(request):
+    return HttpResponse("We are at prodView")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
+
+def search(request):
+    return HttpResponse("We are at search")
