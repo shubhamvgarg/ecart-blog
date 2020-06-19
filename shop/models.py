5b001bbcb538484326d521fad78f31729b379e47
Shubham Garg <shubham07119garg@gmail.com>
2020-06-19 13:52:01 +0530

    Show products on home page

--- shop/models.py
+++ shop/models.py
@@ -7,11 +7,9 @@ class Product(models.Model):
     category = models.CharField(max_length=50, default="")
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
-    desc = models.CharField( max_length=300)
+    desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    images = models.ImageField(upload_to="shop/images", default = "")
+    image = models.ImageField(upload_to='shop/images', default="")
 
     def __str__(self):
         return self.product_name
-
-
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
 # Create your views here.
+from django.http import HttpResponse
+from math import ceil
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all() 
+    print(products)
+    n=len(products)
+    nslides = n//4+ceil((n/4)-(n//4))
+    params = {'no_of_slides':nslides,'range':range(1,nslides),'product':products}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
      return render(request, 'shop/about.html')
