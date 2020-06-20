9d845d592bb784c59d82cf6757ed5b0b0ac35937
Shubham Garg <shubham07119garg@gmail.com>
2020-06-21 00:59:37 +0530

    created product and contact page

--- shop/urls.py
+++ shop/urls.py
@@ -1,17 +1,12 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "ShopHome"), 
-    path("about/", views.about, name = "AboutUs"),
-    path("contact/", views.contact, name = "ContactUs"),
-    path("tracker/", views.tracker, name = "TreakingStatus"),
-    path("search/", views.search, name = "Search"),
-    path("prductview/", views.productView, name = "ProductView"),
-    path("chechout/", views.checkout, name = "Checkout"),
-
-
-
-    
+    path("", views.index, name="ShopHome"),
+    path("about/", views.about, name="AboutUs"),
+    path("contact/", views.contact, name="ContactUs"),
+    path("tracker/", views.tracker, name="TrackingStatus"),
+    path("search/", views.search, name="Search"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
+    path("checkout/", views.checkout, name="Checkout"),
 ]
